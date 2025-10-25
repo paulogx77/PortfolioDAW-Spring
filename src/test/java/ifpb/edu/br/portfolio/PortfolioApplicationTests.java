7e80c5faac5863f8d04ee9c2ad881bc32adfd110
paulogx77 <paulo.guilhermeez@gmail.com>
2025-10-25 09:56:45 -0300

    estrutura inicial

---
+++ src/test/java/ifpb/edu/br/portfolio/PortfolioApplicationTests.java
@@ -0,0 +1,13 @@
+package ifpb.edu.br.portfolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortfolioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
